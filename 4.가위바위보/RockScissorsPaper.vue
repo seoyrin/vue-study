0157a423e44d50595a02581550eb4ea24f44d132
seoyrin <yerinseo97@gmail.com>
2023-06-05 16:29:10 +0900

    기본설정

---
+++ 4.가위바위보/RockScissorsPaper.vue
@@ -0,0 +1,25 @@
+<template>
+    
+</template>
+
+<script>
+    export default { 
+        data() {
+            
+        },
+        computed: {
+            
+        },
+        methods: {
+           
+        },
+    };
+</script>
+
+<style scoped>
+    #computer {
+        width: 142px;
+        height: 200px;
+        background-position: 0 0;
+    }
+</style>
---
+++ 4.가위바위보/webpack.config.js
@@ -0,0 +1,37 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.vue'],
+    },
+    entry: {
+        app: path.join(__dirname, 'main.js'),
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+        }, {
+            test: /\.css$/,
+            use: [
+                'vue-style-loader',
+                'css-loader',
+            ]
+        }],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist'),
+    },
+    devServer: {
+        devMiddleware: { publicPath: '/dist' },
+        static: { directory: path.resolve(__dirname) },
+        hot: true
+    },
+};
